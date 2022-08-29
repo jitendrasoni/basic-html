a4e3765249bb806ef18257fc8ef58579cbd4f95c
jitendrasoniaceik <jitendrasoni@aceik.com.au>
2022-08-29 19:34:19 +1000

    Updated the file format and event listener

--- script/utility.js
+++ script/utility.js
@@ -7,3 +7,12 @@ function showmessage(element)
 {
    alert('The called action is   ' +  element.value  );
 }
+
+//Add event AudioListener
+
+
+document.getElementById("btnListener")?.addEventListener("click", myFunction);
+
+function myFunction() {
+  alert ("Hello World!");
+}
